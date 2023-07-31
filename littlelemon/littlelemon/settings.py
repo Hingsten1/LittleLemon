e2366ffb29aff17155ff8bbc300f4875eade77f0
DJ-Laptop <110897949+Djohan93@users.noreply.github.com>
2023-07-31 17:14:53 +0200

    fixat mysql

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'django-insecure-e0vdtf40yv=2^)r_#3&1ribq*mg+yrg!0)a=uc__@df=n$dx2g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.50.167",]
+ALLOWED_HOSTS = ["192.168.50.167",
+                 "127.0.0.1",
+                 
+                 ]
 
 
 # Application definition
@@ -77,8 +80,15 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "LittleLemon",
+        "USER":"root",
+        "PASSWORD": "GoTackel12",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
+        "OPTIONS": {
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
